6cd2a96087c450e554a332fcdeef01cddfa6caf0
Eqilia Luminé <eqilia@national.shitposting.agency>
2024-08-08 00:10:19 +0200

    init: Initialise repository
    Signed-off-by: Eqilia Luminé <eqilia@national.shitposting.agency>

---
+++ README.md
@@ -0,0 +1,11 @@
+# petbuild!!!
+This is the ports repository for petbuild, the thingie that actually builds
+the ports!
+
+**NOTE**: petbuild is a **VERY** rough prototype right now, it is NOT production ready,
+and in fact, it is so not production ready, if you run it anywhere outside
+of a sealed place, it may or may not meow itself and everything else up.
+
+# License
+The license is the EUPL (European Union Public License v1.2), available in the LICENSE file.
+
---
+++ src/petbuild.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e; # exit on error
+
+shitdir="$(pwd)/build";
+builddir="$shitdir/build";
+pkgdir="$shitdir/pkgs"
+bundledir="$shitdir/bundle";
+fetchdir="$shitdir/fetch"
+
+mkdir -p "$shitdir";
+mkdir -p "$builddir";
+mkdir -p "$pkgdir";
+mkdir -p "$bundledir";
+mkdir -p "$fetchdir";
+
+cmdExists()
+{
+	command -v $1 2>&1 1>/dev/null
+
+	if [ "$?" = "0" ]; then
+		printf "y";
+	else
+		printf "n";
+	fi
+}
+
+. ./PETBUILD
+
+printf "Building %s v%s-r%s\n" "$pkgname" "$pkgver" "$pkgrel";
+
+cd "$fetchdir";
+curl "$pkgtb" > "ugh.tar";
+tar xvf "ugh.tar";
+cd "$pkgname-$pkgver";
+cp -r * "$builddir/";
+
+cd "$builddir";
+if [ "$(cmdExists "configure")" = "y" ]; then
+	configure;
+fi
+
+build
+package
+
+for subpkgname in $subpkgs; do
+	j="package_$(printf "%s\n" "$subpkgname" | sed "s/-/_/g")"
+
+	mkdir -p "$pkgdir/$subpkgname"
+	cd "$pkgdir/$subpkgname"
+
+	petcp()
+	{
+		k="${1%/*}";
+		mkdir -p "$pkgdir/$subpkgname/$k"
+		cp -rv "$bundledir/$1" "$pkgdir/$subpkgname/$k/"
+	}
+
+	"$j";
+
+	tar cpf - * | xz -vvve9 -T1 > "$pkgdir/$subpkgname.tar.xz";
+done
+
